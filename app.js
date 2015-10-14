213e2fd5756bc275c6bbb90786af9fe98fb95643
dyngosz <jestemwiciu@gmail.com>
2015-10-14 16:36:05 +0200

    initial commit

--- app.js
+++ app.js
@@ -5,6 +5,14 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+var mongoose = require('mongoose');                     // mongoose for mongodb
+var bodyParser = require('body-parser');    // pull information from HTML POST (express4)
+
+var mongo = require('mongodb');
+// var monk = require('monk');
+// var mongoUri = process.env.MONGOLAB_URI;
+// var db = monk(mongoUri);
+
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
@@ -21,6 +29,12 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+//mongoose.connect('mongodb://node:nodeuser@mongo.onmodulus.net:27017/uwO3mypu');     // connect to mongoDB database on modulus.io
+
+app.use(express.static(__dirname + '/public'));                 // set the static files location /public/img will be /img for users
+app.use(bodyParser.json());                                     // parse application/json
+app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
 
 app.use('/', routes);
 app.use('/users', users);
@@ -58,3 +72,7 @@ app.use(function(err, req, res, next) {
 
 
 module.exports = app;
+
+// listen (start app with node server.js) ======================================
+    app.listen(8080);
+    console.log("App listening on port 8080");
