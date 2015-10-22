4b1cc1ec8aebb63e6b7c5452a3037b6f64fcbd98
dyngosz <jestemwiciu@gmail.com>
2015-10-22 18:38:50 +0200

    initial project page

--- routes/index.js
+++ routes/index.js
@@ -135,68 +135,113 @@ function profileFunction(req, res){
    collection.findOne({"username" : userName,"userstatus" : {
       $in : ["A","N"]}, "userpassword" : userPassword },function(err, user){
 
-      if(user!=null){
-         if(cookieSet == false) req.session.user = user;
+         if(user!=null){
+            if(cookieSet == false) req.session.user = user;
 
-         var collection2 = db.get('projectcollection');
-         collection2.find({ "projectowner" : userName},function(e,docs2) {
-            var list = [];
-            for (i=0; i<docs2.length; i++) {
+            var collection2 = db.get('projectcollection');
+            collection2.find({ "projectowner" : userName},function(e,docs2) {
+               var list = [];
+               for (i=0; i<docs2.length; i++) {
 
-               list[list.length] = parseInt(docs2[i].projectid);
-            }
+                  list[list.length] = parseInt(docs2[i].projectid);
+               }
 
                res.render('profile', {
                   title: 'Your Profile',
                   "profile" : user,
                   "projects" : docs2,
+               });
             });
-         });
-   }
-   else
-   {
-      var ecom = "Incorrect data. If you don't have an account - Sign up";
-      res.render('index', { "error" : ecom });
-   }
+         }
+         else
+         {
+            var ecom = "Incorrect data. If you don't have an account - Sign up";
+            res.render('index', { "error" : ecom });
+         }
+      });
+   };
+
+   /* GET Creator page. */
+   router.get('/creator', function(req, res) {
+      res.render('creator', { title: 'Project Creator'});
    });
-};
 
-/* GET Creator page. */
-router.get('/creator', function(req, res) {
-   res.render('creator', { title: 'Project Creator'});
-});
+   /* POST to Add Project */
+   router.post('/addproject', function(req, res) {
 
-/* POST to Add Project */
-router.post('/addproject', function(req, res) {
+      var username = req.session.user.username;
+      var projectname = req.body.projectname;
 
-   var username = req.session.user.username;
-   var projectname = req.body.projectname;
+      var db = req.db;
+      var collection = db.get('projectcollection');
+      collection.count({},function(err, count){
+         projectid = count+1;
+         // Submit to the DB
+         collection.insert({
+            "projectname" : projectname,
+            "projectowner" : username,
+            "projectid" : projectid
+         }, function (err, doc) {
+            if (err) {
+               // If it failed, return error
+               var ecom = "There was a problem during adding the information to the database.";
+               res.render('errorpage', { "error" : ecom, "page" : "/profile" });
+               return;
+            }
+            else {
+               // If it worked, set the header so the address bar doesn't still say /adduser
+               res.location("/profile");
+               // And forward to success page
+               res.redirect("/profile");
+            }
+         });
+      });
 
-   var db = req.db;
-   var collection = db.get('projectcollection');
-   collection.count({},function(err, count){
-      projectid = count+1;
-      // Submit to the DB
-      collection.insert({
-         "projectname" : projectname,
-         "projectowner" : username,
-         "projectid" : projectid
-      }, function (err, doc) {
-         if (err) {
-            // If it failed, return error
-            var ecom = "There was a problem during adding the information to the database.";
-            res.render('errorpage', { "error" : ecom, "page" : "/profile" });
-            return;
-         }
-         else {
-            // If it worked, set the header so the address bar doesn't still say /adduser
-            res.location("/profile");
-            // And forward to success page
-            res.redirect("/profile");
-         }
+   });
+
+   /*Link to project*/
+   router.get('/project', function(req, res){
+
+
+      var projectid = req.query['id'];
+      if(projectid == undefined || projectid == ""){
+         var ecom = "There is no project with this number";
+         res.render('errorpage', { "error" : ecom, "page" : "/profile" });
+         return;
+      }
+      var db = req.db;
+      // Get our form values. These rely on the "name" attributes
+      if(!req.session.user){
+         // If it worked, set the header so the address bar doesn't still say /adduser
+         res.location("/");
+         // And forward to success page
+         res.redirect("/?project="+projectid);
+         return;
+      }
+      else {
+         var userName = req.session.user.username;
+         var userPassword = req.session.user.userpassword;
+      }
+      // Set our collection
+      var collection = db.get('usercollection');
+
+      collection.find({"username" : userName,"userstatus" : {$in : ["A","N"]},
+      "userpassword" : userPassword },function(err, find){
+            var collection2 = db.get('projectcollection');
+
+            collection2.find({"projectid" : parseInt(projectid)}, function(err,doc){
+
+               if(doc[0]==undefined || doc[0]==""){
+                  var ecom = "Wrong number of project";
+                  res.render('errorpage', { "error" : ecom, "page" : "/profile" });
+                  return;
+               }
+               res.render('project', {
+                  "projectid" : projectid,
+               });
+            });
       });
    });
 
-});
 
-module.exports = router;
+   module.exports = router;
